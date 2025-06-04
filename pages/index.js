a0d46d475ccb02009ce8612f6a7af5f6997c60ce
cicicas <cintiacasemiro@yahoo.com.br>
2025-06-04 15:50:04 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div style={{ height: '100vh', display: 'grid', placeItems: 'center', backgroundColor: '#000', color: '#fff' }}>
-      <h1>cria.art digital – experiências visuais que marcam</h1>
+    <div style={{ height: '100vh', display: 'grid', placeItems: 'center', backgroundColor: '#000', color: '#fff', textAlign: 'center' }}>
+      <div>
+        <h1 style={{ fontSize: '2rem' }}>cria.art digital</h1>
+        <p>Projeção | Audiovisual | Som | Imagens</p>
+        <p>Experiências visuais que marcam.</p>
+      </div>
     </div>
   );
 }
